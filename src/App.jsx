cebfe9c360592c1471a0bea367792332ad17c503
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-08-01 04:05:41 +0530

    changed code

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import { Routes, Route } from 'react-router-dom';
+
+import Header from './components/header/Header';
+import Home from './pages/home/Home';
+import CountryDetail from './pages/country-detail/CountryDetail';
+
+const App = () => {
+  return <main className="main-container">
+    <Routes>
+      <Route exact path="/" element={<Header />} >
+        <Route index element={<Home />} />
+        <Route path="/:code" element={<CountryDetail />} />
+
+      </Route>
+    </Routes>
+  </main>;
+};
+
+export default App;
