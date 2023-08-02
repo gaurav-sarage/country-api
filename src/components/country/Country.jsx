dd21e87dbf6b66982fa7b2d1af32dd57ac09d5ba
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-08-02 23:15:41 +0530

    a function and hook to manage the data coming from the api

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,27 +1,55 @@
 import "./country.css";
+import { useState, useEffect } from "react";
+
+// Redux
+import { UseSelector, useDispatch, useSelector } from "react-redux";
+import { showAllCountries } from "../../features/countries/countriesAction";
+import { reset } from '../../features/countries/countriesSlice';
 
 const Country = () => {
+  const [countriesData, loading, success, error] = useSelector((state) => state.country);
+
+  const dispatch = useDispatch();
+
+  const [countryData, setCountryData] = useState([]);
+
+  useEffect(() => {
+    dispatch(showAllCountries);
+
+    if(success) {
+      setCountryData(countriesData);
+    }
+
+
+    if(error) {
+      console.log(error);
+    }
+  }, [dispatch, error, success])
+
   return (
     <section className="country-container">
-      <div
-        // onClick={() => dispatch(searchByName(item.cioc.toLowerCase()))}
-        className="country-card"
-        key=""
-      >
-        <img src="#" alt="" className="country-image" />
-        <div className="country-content">
-          <h3> </h3>
-          <p>
-            Population: <span></span>
-          </p>
-          <p>
-            Region: <span></span>
-          </p>
-          <p>
-            Capital: <span></span>
-          </p>
-        </div>
-      </div>
+      {loading ? (<h1>Loading...</h1>) : (
+        countryData.length > 0 && countryData.map((item, index) => {
+          return (
+            <div className="country-card" key="">
+              <img src="#" alt="" className="country-image" />
+                <div className="country-content">
+                  <h3> </h3>
+                  <p>
+                    Population: <span></span>
+                  </p>
+                  <p>
+                    Region: <span></span>
+                  </p>
+                  <p>
+                    Capital: <span></span>
+                  </p>
+                </div>
+            </div>
+          )
+        }) 
+      )}
+      
     </section>
   );
 };
