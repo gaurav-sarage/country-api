29fb40d8e3630b48ae5a8f26dd1ffdaa2c946346
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-08-02 22:50:54 +0530

    installed redux and imported in index file with store as the parameter

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { store } from './app/store';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <Provider store={store}>
       <App />
+    </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
