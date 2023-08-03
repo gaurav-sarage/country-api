710ecaa39cbbcb4c1394cfcbf2755e05572f600b
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-08-03 22:59:14 +0530

    completed adding the country component

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,43 +1,68 @@
-import './country.css';
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import "./country.css";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-// Redux
-import { useSelector, useDispatch } from 'react-redux';
+//redux
+import { useSelector, useDispatch } from "react-redux";
 import {
   searchByRegion,
-  showAllCountries
-} from '../../features/countries/countriesAction';
-import { reset } from '../../features/countries/countriesSlice';
+  showAllCountries,
+} from "../../features/countries/countriesAction";
+import { reset } from "../../features/countries/countriesSlice";
 
 const Country = () => {
-  const { countriesData, loading, success, error, region, searchTerm } = useSelector((state) => state.country);
+  const { countriesData, loading, success, error, region, searchTerm } =
+    useSelector((state) => state.country);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(showAllCountrie());
+    dispatch(showAllCountries());
 
-    if(region) {
+    if (region) {
       dispatch(searchByRegion(region));
     }
 
-    if(error) {
+    if (error) {
       console.log(error);
     }
   }, [dispatch, error, success, region]);
 
-  const data = countriesData.filter((item) => item.name.common.toLowerCase().includes(searchTerm));
+  const data = countriesData.filter((item) =>
+    item.name.common.toLowerCase().includes(searchTerm)
+  );
 
   return (
-    <section className='country-container'>
+    <section className="country-container">
       {loading ? (
         <h1>Loading...</h1>
       ) : (
         data.length > 0 &&
         data.map((item, index) => {
-          
+          return (
+            <Link className="country-card" key={index} to={`/${item.cioc}`}>
+              <img
+                src={item.flags.png}
+                alt={item.flags.alt}
+                className="country-image"
+              />
+              <div className="country-content">
+                <h3>{item.name.common} </h3>
+                <p>
+                  Population: <span>{item.population}</span>
+                </p>
+                <p>
+                  Region: <span>{item.region}</span>
+                </p>
+                <p>
+                  Capital: <span>{item.capital}</span>
+                </p>
+              </div>
+            </Link>
+          );
         })
       )}
     </section>
-  )
-}
+  );
+};
+
+export default Country;
