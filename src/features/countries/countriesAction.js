d669e7256443ade059ad749be4fda58ae6d1f1cd
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-08-03 23:46:12 +0530

    added function to search country by region

--- src/features/countries/countriesAction.js
+++ src/features/countries/countriesAction.js
@@ -1,39 +1,57 @@
-import axios from 'axios';
-import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 
 // show all countries
-
-export const showAllCountries = createAsyncThunk('countries/showAll', async(_, thunkAPI) => {
+export const showAllCountries = createAsyncThunk(
+  "countries/showAll",
+  async (_, thunkAPI) => {
     try {
-        const response = await axios.get(`https://restcountries.com/v3.1/all`);
+      const response = await axios.get(`https://restcountries.com/v3.1/all`);
+      //   console.log(response.data);
+      return response.data;
+    } catch (err) {
+      const message = (err.response && err.response.data) || err.message;
 
-        return response.data;
-
-    } catch(err) {
-        const message = (err.response && err.response.data) || err.message;
-
-        // reject with value sends an error msg as a payload
-        return thunkAPI.rejectWithValue(message);
+      // rejectWithValue sends the error message as a payload
+      return thunkAPI.rejectWithValue(message);
     }
-});
+  }
+);
 
-
-
-
+// search by cioc code
 export const searchByCode = createAsyncThunk(
-    "countries/searchByCode",
-    async (code, thunkAPI) => {
-      try {
-        const response = await axios.get(
-          `https://restcountries.com/v3.1/alpha/${code}`
-        );
-  
-        return response.data;
-      } catch (err) {
-        const message = (err.response && err.response.data) || err.message;
-  
-        // rejectWithValue sends the error message as a payload
-        return thunkAPI.rejectWithValue(message);
-      }
+  "countries/searchByCode",
+  async (code, thunkAPI) => {
+    try {
+      const response = await axios.get(
+        `https://restcountries.com/v3.1/alpha/${code}`
+      );
+
+      return response.data;
+    } catch (err) {
+      const message = (err.response && err.response.data) || err.message;
+
+      // rejectWithValue sends the error message as a payload
+      return thunkAPI.rejectWithValue(message);
     }
-  );
+  }
+);
+
+// search by region
+export const searchByRegion = createAsyncThunk(
+  "countries/searchByRegion",
+  async (region, thunkAPI) => {
+    try {
+      const response = await axios.get(
+        `https://restcountries.com/v3.1/region/${region}`
+      );
+
+      return response.data;
+    } catch (err) {
+      const message = (err.response && err.response.data) || err.message;
+
+      // rejectWithValue sends the error message as a payload
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
