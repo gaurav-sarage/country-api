dc8bdab64fbb61a379e1d7338145fce6ec607d6b
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-08-04 23:37:15 +0530

    added functional code to display the country details once clicked upon from the home page

--- src/pages/country-detail/CountryDetail.jsx
+++ src/pages/country-detail/CountryDetail.jsx
@@ -1,58 +1,118 @@
 import "./country-detail.css";
+import { useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+
+//Redux
+import { useSelector, useDispatch } from "react-redux";
+import { searchByCode } from "../../features/countries/countriesAction";
+import { reset } from "../../features/countries/countriesSlice";
 
 const CountryDetail = () => {
+  const { loading, error, countrySearched } = useSelector(
+    (state) => state.country
+  );
+
+  const dispatch = useDispatch();
+  const { code } = useParams();
+
+  useEffect(() => {
+    if (code) {
+      dispatch(searchByCode(code.toLowerCase()));
+    }
+
+    if (error) {
+      console.log(error);
+    }
+
+    return () => {
+      dispatch(reset());
+    };
+  }, [dispatch, code, error]);
   return (
     <section className="country-detail-container">
-      <div className="back-button" to="/">
+      <Link className="back-button" to="/">
         <i className="fa-solid fa-arrow-left"></i> Back
-      </div>
+      </Link>
 
       <div className="country-detail-content">
-        <>
-          <img src="#" alt="name" className="country-detail-image" />
+        {countrySearched.length > 0 ? (
+          <>
+            <img
+              src={countrySearched[0].flags.png}
+              alt={countrySearched[0].flags.alt}
+              className="country-detail-image"
+            />
 
-          <div className="country-detail-right">
-            <div className="details">
-              <div className="detail-left">
-                <p>
-                  Offcial Name: <span>{}</span>
-                </p>
-                <p>
-                  Population: <span>{}</span>
-                </p>
-                <p>
-                  Region: <span>{}</span>
-                </p>
+            <div className="country-detail-right">
+              <h1>{countrySearched[0].name.common}</h1>
+              <div className="details">
+                <div className="detail-left">
+                  <p>
+                    Offcial Name:{" "}
+                    <span>{countrySearched[0].name.official}</span>
+                  </p>
+                  <p>
+                    Population: <span>{countrySearched[0].population}</span>
+                  </p>
+                  <p>
+                    Region: <span>{countrySearched[0].region}</span>
+                  </p>
 
-                <p>
-                  Sub Region: <span>{}</span>
-                </p>
-                <p>
-                  Capital: <span>{}</span>
-                </p>
+                  <p>
+                    Sub Region: <span>{countrySearched[0].subregion}</span>
+                  </p>
+                  <p>
+                    Capital: <span>{countrySearched[0].capital}</span>
+                  </p>
+                </div>
+
+                <div className="detail-right">
+                  <p>
+                    Top Level Domain: <span>{countrySearched[0].tld[0]}</span>
+                  </p>
+                  <p>
+                    Currencies:
+                    <span>
+                      {Object.values(countrySearched[0].currencies)
+                        .map((item) => {
+                          return item.name;
+                        })
+                        .join(",")}
+                    </span>
+                  </p>
+
+                  <p>
+                    Languages:
+                    <span>
+                      {Object.values(countrySearched[0].languages)
+                        .map((item) => {
+                          return item;
+                        })
+                        .join(",")}
+                    </span>
+                  </p>
+                </div>
               </div>
 
-              <div className="detail-right">
-                <p>
-                  Top Level Domain: <span>{}</span>
-                </p>
-                <p>
-                  Currencies:
-                  <span></span>
-                </p>
-
-                <p>
-                  Languages:
-                  <span></span>
-                </p>
+              <div className="border">
+                <p>Border Countries:</p>
+                {countrySearched[0].borders ? (
+                  countrySearched[0].borders.map((item, index) => {
+                    return (
+                      <Link className="border-name" to={`/${item}`} key={index}>
+                        <p>{item}</p>
+                      </Link>
+                    );
+                  })
+                ) : (
+                  <span>No Borders</span>
+                )}
               </div>
             </div>
-
-            <div className="border">
-              <p>Border Countries:</p>
-            </div>
-          </div>
-        </>
+          </>
+        ) : (
+          <div>No details found</div>
+        )}
       </div>
     </section>
   );
