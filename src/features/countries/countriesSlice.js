a10f86f9fc6404ad12cbfdbcac41717f89b50c14
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-08-04 14:23:18 +0530

    indented the code

--- src/features/countries/countriesSlice.js
+++ src/features/countries/countriesSlice.js
@@ -1,88 +1,86 @@
 import { createSlice } from "@reduxjs/toolkit";
 import {
-    showAllCountries,
-    searchByCode,
-    searchByRegion
-} from './countriesAction';
+  showAllCountries,
+  searchByCode,
+  searchByRegion,
+} from "./countriesAction";
 
 const initialState = {
-    loading: false,
-    countriesData: [],
-    countrySearched: [],
-    region: '',
-    searchTerm: '',
-    error: false,
-    success: false,
-    message: ''
+  loading: false,
+  countriesData: [],
+  countrySearched: [],
+  region: "",
+  searchTerm: "",
+  error: false,
+  success: false,
+  message: "",
 };
 
 export const countriesSlice = createSlice({
-    name: 'countries',
-    initialState: initialState,
-    reducers: {
-        reset: (state) => {
-            state.loading = false;
-            state.success = false;
-            state.error = false;
-            state.message = '',
-            state.countrySearched = [],
-            state.region = ''
-        },
-        setRegion: (state, action) => {
-            state.region = action.payload;
-        },
+  name: "countries",
+  initialState: initialState,
+  reducers: {
+    reset: (state) => {
+      state.loading = false;
+      state.success = false;
+      state.error = false;
+      state.message = "";
+      state.countrySearched = [];
+      state.region = "";
+    },
+    setRegion: (state, action) => {
+      state.region = action.payload;
+    },
 
-        setSearchTerm: (state, action) => {
-            state.searchTerm = action.payload;
-        },
+    setSearchTerm: (state, action) => {
+      state.searchTerm = action.payload;
     },
-    extraReducers: (builder) => {
-        builder
-            .addCase(showAllCountries.fulfilled, (state, action) => {
-                state.loading = false;
-                state.countriesData = action.payload;
-                state.success = true;
-            })
-            .addCase(showAllCountries.rejected, (state, action) => {
-                state.loading = false;
-                state.countriesData = [],
-                state.error = true;
-                state.message = action.payload;
-            })
-            .addCase(searchByCode.pending, (state) => {
-                state.loading = true;
-            })
-            .addCase(searchByCode.fulfilled, (state, action) => {
-                state.loading = false;
-                state.countrySearched = action.payload;
-                state.success = true;
-            })
-            .addCase(searchByCode.fulfilled, (state, action) => {
-                state.loading = false;
-                state.countrySearched = action.payload;
-                state.success = true;
-            })
-            .addCase(searchByCode.rejected, (state, action) => {
-                state.loading = false;
-                state.error = true;
-                state.message = action.payload;
-                state.countrySearched = [];
-            })
-            .addCase(searchByRegion.pending, (state) => {
-                state.loading = true;
-            })
-            .addCase(searchByRegion.fulfilled, (state, action) => {
-                state.loading = false;
-                state.countriesData = action.payload;
-                state.success = true;
-            })
-            .addCase(searchByRegion.rejected, (state, action) => {
-                state.loading = false;
-                state.error = true;
-                state.message = action.payload;
-                state.countriesData = []
-            });
-    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(showAllCountries.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(showAllCountries.fulfilled, (state, action) => {
+        state.loading = false;
+        state.countriesData = action.payload;
+        state.success = true;
+      })
+      .addCase(showAllCountries.rejected, (state, action) => {
+        state.loading = false;
+        state.countriesData = [];
+        state.error = true;
+        state.message = action.payload;
+      })
+      .addCase(searchByCode.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(searchByCode.fulfilled, (state, action) => {
+        state.loading = false;
+        state.countrySearched = action.payload;
+        state.success = true;
+      })
+      .addCase(searchByCode.rejected, (state, action) => {
+        state.loading = false;
+        state.error = true;
+        state.message = action.payload;
+        state.countrySearched = [];
+      })
+      .addCase(searchByRegion.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(searchByRegion.fulfilled, (state, action) => {
+        state.loading = false;
+        state.countriesData = action.payload;
+        state.success = true;
+      })
+      .addCase(searchByRegion.rejected, (state, action) => {
+        state.loading = false;
+        state.error = true;
+        state.message = action.payload;
+        state.countriesData = [];
+      });
+  },
 });
 
 export const { reset, setRegion, setSearchTerm } = countriesSlice.actions;
